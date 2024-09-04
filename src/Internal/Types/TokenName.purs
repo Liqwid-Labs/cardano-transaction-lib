ad923ffd68796a2b6963dea98f6d04761cf35342
Przemysław Kopański <przemek@liqwid.finance>
2024-09-04 23:12:05 +0200

    Use hex encoding of TokenName

--- src/Internal/Types/TokenName.purs
+++ src/Internal/Types/TokenName.purs
@@ -25,17 +25,15 @@ import Ctl.Internal.Metadata.ToMetadata (class ToMetadata)
 import Ctl.Internal.Serialization.Types (AssetName) as CSL
 import Ctl.Internal.ToData (class ToData)
 import Ctl.Internal.Types.ByteArray (ByteArray, byteArrayToHex, byteLength)
-import Ctl.Internal.Types.RawBytes (RawBytes(RawBytes))
+import Ctl.Internal.Types.RawBytes (RawBytes)
 import Data.ArrayBuffer.Types (Uint8Array)
 import Data.BigInt (BigInt)
 import Data.Bitraversable (ltraverse)
-import Data.Either (Either(Right, Left), either, note)
+import Data.Either (Either(Left), note)
 import Data.Map (Map)
 import Data.Map (fromFoldable) as Map
 import Data.Maybe (Maybe(Nothing), fromJust)
 import Data.Newtype (unwrap, wrap)
-import Data.String.CodePoints (drop, take)
-import Data.TextEncoder (encodeUtf8)
 import Data.Traversable (class Traversable, traverse)
 import Data.Tuple.Nested (type (/\))
 import Partial.Unsafe (unsafePartial)
@@ -57,39 +55,18 @@ instance Arbitrary TokenName where
 foreign import _decodeUtf8
   :: forall (r :: Type). Uint8Array -> (String -> r) -> (String -> r) -> r
 
-fromTokenName
-  :: forall (r :: Type). (ByteArray -> r) -> (String -> r) -> TokenName -> r
-fromTokenName arrayHandler stringHandler (TokenName (RawBytes ba)) = either
-  (const $ arrayHandler $ ba)
-  stringHandler
-  (_decodeUtf8 (unwrap ba) Left Right)
-
--- | Corresponds to the Haskell instance at https://github.com/input-output-hk/plutus/blob/4fd86930f1dc628a816adf5f5d854b3fec578312/plutus-ledger-api/src/Plutus/V1/Ledger/Value.hs#L155:
+-- | Corresponds to CurrencySymbol
 instance DecodeAeson TokenName where
-  decodeAeson = caseAesonObject (Left $ TypeMismatch "Expected object") $
-    \aes -> do
-      tkstr <- getField aes "unTokenName"
-      case take 3 tkstr of
-        "\x0000000x" -> do -- this is 3 characters '\NUL' '0' 'x'
-          let stripped = drop 3 tkstr -- strip the \NUL followed by "0x"
-          ba <-
-            note
-              (TypeMismatch $ "Expected base16 encoded string got " <> stripped)
-              $ hexToByteArray stripped
-          pure $ TokenName (wrap ba)
-        "\x0\x0\x0" -> Right $ tkFromStr (drop 2 tkstr) -- if the original started with \NUL, we prepended 2 additional \NULs
-        _ -> Right $ tkFromStr tkstr
-    where
-    tkFromStr :: String -> TokenName
-    tkFromStr = TokenName <<< wrap <<< wrap <<< encodeUtf8
+  decodeAeson = caseAesonObject
+    (Left $ TypeMismatch "Expected object")
+    ( note (TypeMismatch "Invalid TokenName") <<< mkTokenName
+        <=< note (TypeMismatch "Invalid ByteArray") <<< hexToByteArray
+        <=< flip getField "unTokenName"
+    )
 
 instance EncodeAeson TokenName where
-  encodeAeson = encodeAeson <<< { "unTokenName": _ } <<< fromTokenName
-    (\ba -> "\x0" <> "0x" <> byteArrayToHex ba)
-    ( \s -> case take 1 s of
-        "\x0" -> "\x0\x0" <> s
-        _ -> s
-    )
+  encodeAeson (TokenName ba) = encodeAeson
+    { "unTokenName": byteArrayToHex (unwrap ba) }
 
 instance Show TokenName where
   show (TokenName tn) = "(TokenName " <> show tn <> ")"
