5628d6a88080f558101d6204b94a81cc9fb2cc47
GRoscoe <gustavo@gustavoroscoe.com>
2024-12-17 16:31:38 -0300

    Strip out the 258 cbor set tags from the return of signTx

--- src/Internal/Wallet/Cip30.js
+++ src/Internal/Wallet/Cip30.js
@@ -1,4 +1,7 @@
 /* global BROWSER_RUNTIME */
+const cbor2 = require('cbor2');
+const cbor2Encoder = require('cbor2/encoder');
+const cbor2Utils = require('cbor2/utils');
 
 exports._getNetworkId = conn => () => conn.getNetworkId();
 
@@ -26,7 +29,15 @@ exports._getChangeAddress = conn => () => conn.getChangeAddress();
 exports._getRewardAddresses = conn => () => conn.getRewardAddresses();
 
 exports._signTx = txHex => conn => () =>
-  conn.signTx(txHex, true).catch(e => {
+  conn.signTx(txHex, true).then(hexEncodedWitnessSet => {
+    // Decode the witness set and re-encode it stripping out the set tag (258).
+    const decoded = cbor2.decode(cbor2Utils.hexToU8(hexEncodedWitnessSet));
+    const previousSetEncoder = cbor2Encoder.registerEncoder(Set, (obj) => [undefined, [...obj]]);
+    const encodedWithoutSetTag = cbor2.encode(decoded);
+    // Restore the previous encoder for Sets.
+    cbor2Encoder.registerEncoder(Set, previousSetEncoder);
+    return cbor2Utils.u8toHex(encodedWithoutSetTag);
+  }).catch(e => {
     throw JSON.stringify(e);
   });
 
